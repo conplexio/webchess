585c164579c5cc077cd43ba3938da7e2c807366e
conplexio <156047506+conplexio@users.noreply.github.com>
2024-01-12 00:55:23 +0100

    httpd running

--- src/webchess_app.erl
+++ src/webchess_app.erl
@@ -5,9 +5,21 @@
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-    webchess_sup:start_link().
+	io:format("webchess_app: start~n"),
+	Httpd_conf = [
+		{port, 3333},
+		{server_name,"webchess"},
+		{server_root,"../httpd"},
+		{document_root,"../httpd/htdocs"},
+		{bind_address,  "localhost"}
+	],
+	inets:start(permanent),
+	{ok, Httpd} = inets:start(httpd, Httpd_conf),
+    {ok, Webchess} = webchess_sup:start_link(),
+	{ok, Webchess, #{httpd => Httpd}}.
 
-stop(_State) ->
-    ok.
+stop(#{httpd := Httpd}) ->
+	io:format("webchess_app: stop~n"),
+	inets:stop(httpd, Httpd).
 
 
--- src/webchess_sup.erl
+++ src/webchess_sup.erl
@@ -13,12 +13,11 @@ init(_Args) ->
 	io:format("webchess_sup: init~n"),
 	SupFlags = #{
 		strategy => one_for_one,
-%		auto_shutdown => never,
+%		auto_shutdown => never, % from OTP 24.
 		intensity => 4,
 		period => 3600
 	},
 	ChildSpecs = [
-	%	#{id := httpd_sup,
 		#{id => test_sup,
 			start => {test_sup, start_link, []},
 			restart => temporary,
